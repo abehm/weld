e0dbab33d750a25108da8b4f1131b3e92fca05ee
Ubuntu <ubuntu@ip-172-31-36-230.us-west-2.compute.internal>
2018-04-17 01:07:38 +0000

    more demo files

---
+++ examples/python/plot_utils.py
@@ -0,0 +1,25 @@
+from matplotlib import pyplot as plt
+import seaborn as sns
+sns.set_style('ticks')
+sns.set_palette('Paired', 10)
+sns.set_context('talk')
+sns.set(font_scale=2)
+
+# Helper function to plot results.
+def plot_results(times, title):
+    plt.figure(figsize=(15, 8))
+    xstart = 0.0
+    ticks = []
+    labels = []
+    data_sorted = sorted(times.iteritems())
+    data_sorted.reverse()
+    for (time, scheme) in data_sorted:
+        plt.bar(xstart, time, label=scheme)
+        labels.append(scheme)
+        ticks.append(xstart)
+        xstart += 0.8
+    plt.title(title)
+    plt.ylabel("Time (in seconds)")
+    plt.xlabel("Scheme")
+    plt.xticks(ticks, labels)
+    plt.show()
